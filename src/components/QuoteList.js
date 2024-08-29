dc856c95474fe03aa865c1278b84e69d604c4117
gowda26 <41117984+gowda26@users.noreply.github.com>
2024-08-29 13:23:20 +0530

    Add files via upload

--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -7,6 +7,13 @@ const QuoteList = () => {
     { text: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
     { text: "Success is not the key to happiness. Happiness is the key to success.", author: "Albert Schweitzer" },
     { text: "Don't watch the clock; do what it does. Keep going.", author: "Sam Levenson" },
+    { text: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" },
+    { text: "You miss 100% of the shots you don’t take.", author: "Wayne Gretzky" },
+    { text: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" },
+    { text: "Act as if what you do makes a difference. It does.", author: "William James" },
+    { text: "Your time is limited, so don’t waste it living someone else’s life.", author: "Steve Jobs" },
+    { text: "The best way to predict the future is to create it.", author: "Peter Drucker" },
+    { text: "It always seems impossible until it's done.", author: "Nelson Mandela" },
   ];
 
   return (
